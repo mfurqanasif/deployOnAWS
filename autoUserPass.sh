a22257619495632d7de9bf25a585b1ae41f25df4
Ubuntu <ubuntu@ip-172-31-22-242.eu-north-1.compute.internal>
2023-11-30 11:27:50 +0000

    adding task 2 on gitHub using bash file

---
+++ autoUserPass.sh
@@ -0,0 +1,16 @@
+#!/bin/bash/
+
+echo "Task 2"
+echo "Creating folder "
+mkdir Task_2
+echo "changing dir"
+echo  "creating file "
+cd Task_2
+touch demoFile.txt
+echo "file has been created " > demoFile.txt
+cd ..
+git add . 
+git commit -m "adding task 2 on gitHub using bash file"
+git push origin main
+<mfurqanasif
+<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
