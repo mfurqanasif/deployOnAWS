97e92f0034ccde7cae215bbca6a20178f401995f
Ubuntu <ubuntu@ip-172-31-22-242.eu-north-1.compute.internal>
2023-11-30 11:17:17 +0000

    adding task 2 on gitHub using bash file

---
+++ autoPush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash/
+
+echo "Task 2"
+echo" Creating folder "
+mkdir Task_2
+echo "changing dir"
+echo  "creating file "
+cd Task_2
+touch demoFile.txt
+echo "file has been created " > demoFile.txt
+cd ..
+git add . 
+git commit -m "adding task 2 on gitHub using bash file"
+git push origin main
